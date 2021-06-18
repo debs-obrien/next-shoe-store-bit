245e9840805df080b2fa4de866669ac075abbc8f
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-06-18 15:01:22 +0200

    testing transpile of multiple components

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withTM = require('next-transpile-modules')([
+  '@learn-bit-react/base-ui.styles.button',
+  '@learn-bit-react/base-ui.ui.heading',
+  '@learn-bit-react/base-ui.ui.button',
+  // '@learn-bit-react/shoe-store.ui.pages.home-shoes'
+  '@learn-bit-react/ecommerce.ui.header'
+]);
+module.exports = withTM({
+  reactStrictMode: true
+});
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import { Heading } from '@learn-bit-react/base-ui.ui.heading';
+import { Button } from '@learn-bit-react/base-ui.ui.button';
+// import { HomePage } from '@learn-bit-react/shoe-store.ui.pages.home-shoes';
+import { Header } from '@learn-bit-react/ecommerce.ui.header';
 export default function Home() {
   return (
     <div>
+      <Header />
       <Heading>Hello</Heading>
+      <Button buttonText="click" />
+      {/* <HomePage /> */}
     </div>
   );
 }
